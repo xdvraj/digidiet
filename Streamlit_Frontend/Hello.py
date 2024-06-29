8dec05eb1409d206a3f8361d465a0a318b296ddd
VRAJ <139940705+xdvraj@users.noreply.github.com>
2024-06-29 19:27:23 +0530

    SOME IMPORTANT FILES

---
+++ Streamlit_Frontend/Hello.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello Freak",
+    page_icon="👋",
+)
+
+st.write("# Warm welcome to DigiDiet 👋❤️")
+
+st.sidebar.success("Select a recommendation app.")
+
+st.markdown(
+    """
+    Transform your health with DigiDiet: Personalized nutrition plans for a vibrant and balanced life.
+    """
+)
